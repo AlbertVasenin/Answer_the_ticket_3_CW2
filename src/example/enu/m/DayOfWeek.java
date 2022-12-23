c10fd2bae27a2045dc778353e5dd2192dc2a79d5
Albert <albert_vasenin@mail.ru>
2022-12-24 00:36:18 +0400

    questions from the ticket 3 coursework 2

---
+++ src/example/enu/m/DayOfWeek.java
@@ -0,0 +1,20 @@
+package example.enu.m;
+
+public enum DayOfWeek {
+  MONDAY("Понедельник"),
+  TUESDAY("Вторник"),
+  WEDNESDAY("Среда"),
+  THURSDAY("Четверг"),
+  FRIDAY("Пятница"),
+  SATURDAY("Суббота"),
+  SUNDAY("Воскресенье");
+  private String name;
+
+  DayOfWeek(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
